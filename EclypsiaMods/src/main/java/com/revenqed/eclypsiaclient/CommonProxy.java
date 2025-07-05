53c9bcdb807df70bcbdb0d3e25c531bc9a6183e7
Vaelz-dev <vaelz.dev@gmail.com>
2025-07-05 18:14:39 +0200

    modsEc.zip

---
+++ EclypsiaMods/src/main/java/com/revenqed/eclypsiaclient/CommonProxy.java
@@ -0,0 +1,8 @@
+package com.revenqed.eclypsiaclient;
+
+public class CommonProxy implements IProxy {
+    @Override
+    public void init() {
+        // Rien ici, jamais appelé côté client
+    }
+}
